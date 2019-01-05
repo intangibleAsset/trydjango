df3d3e1ac743709383f8d5ff5edc2e45f4e0ee38
intangibleAsset <pingsucceeded@googlemail.com>
2019-01-05 19:03:56 +0000

    Tutorial 24

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', views.product_detail_view, name='product_detail_view'),
+    path('create/', views.product_create_view, name='product_create_view'),
     path('', views.home_view, name='home'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,15 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from . models import Product
+from . forms import ProductForm
 # Create your views here.
 def home_view(request):
     return render(request, "products/start_page.html",{"title":"home page"})
 
 def product_detail_view(request):
-    obj = Product.objects.get(id=1)
+    obj = Product.objects.get(id=5)
 #    context = {
 #    'title': obj.title,
 #    'description': obj.description
 #    }
     context = {"object":obj}
     return render(request,"products/detail.html",context)
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context = {
+        "form":form
+    }
+    return render(request,"products/product_create.html",context)
