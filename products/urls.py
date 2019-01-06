cecf6b047fb95cf550046e6eedff1ac53a2ff534
intangibleAsset <pingsucceeded@googlemail.com>
2019-01-06 19:49:37 +0000

    tutorial 29

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/<int:my_id>/', views.dynamic_lookup_view, name='dynamic_lookup_view'),
     path('products/', views.product_detail_view, name='product_detail_view'),
     path('create/', views.product_create_view, name='product_create_view'),
     path('', views.home_view, name='home'),
--- products/views.py
+++ products/views.py
@@ -7,7 +7,7 @@ def home_view(request):
     return render(request, "products/start_page.html",{"title":"home page"})
 
 def product_detail_view(request):
-    obj = Product.objects.get(id=5)
+    obj = Product.objects.get(id=6)
 #    context = {
 #    'title': obj.title,
 #    'description': obj.description
@@ -15,11 +15,18 @@ def product_detail_view(request):
     context = {"object":obj}
     return render(request,"products/detail.html",context)
 
+def dynamic_lookup_view(request,my_id):
+    obj = Product.objects.get(id=my_id)
+    context = {"object":obj}
+    return render(request,"products/detail.html",context)
+
+
 def product_create_view(request):
     form = ProductForm(request.POST or None)
     if form.is_valid():
         form.save()
 
+    print(request.POST)
     context = {
         "form":form
     }
