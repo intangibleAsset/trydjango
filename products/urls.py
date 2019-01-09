eb4165ae4d2e2182ffbe4353653a86067312fde1
intangibleAsset <pingsucceeded@googlemail.com>
2019-01-09 18:00:54 +0000

    tutorial 32

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/product_list_view', views.product_list_view, name='product_list_view'),
+    path('products/<int:id>/delete/',views.product_delete_view, name='product_delete'),
     path('products/<int:my_id>/', views.dynamic_lookup_view, name='dynamic_lookup_view'),
     path('products/', views.product_detail_view, name='product_detail_view'),
     path('create/', views.product_create_view, name='product_create_view'),
--- products/views.py
+++ products/views.py
@@ -1,10 +1,24 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 from . models import Product
 from . forms import ProductForm
 # Create your views here.
 def home_view(request):
     return render(request, "products/start_page.html",{"title":"home page"})
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+    "object_list": queryset
+    }
+    return render(request, "products/product_list.html", context)
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('../')
+    context = {"object":obj}
+    return render(request, 'products/product_delete.html',context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=6)
@@ -16,7 +30,7 @@ def product_detail_view(request):
     return render(request,"products/detail.html",context)
 
 def dynamic_lookup_view(request,my_id):
-    obj = Product.objects.get(id=my_id)
+    obj = get_object_or_404(Product,id=my_id)
     context = {"object":obj}
     return render(request,"products/detail.html",context)
 
@@ -25,6 +39,8 @@ def product_create_view(request):
     form = ProductForm(request.POST or None)
     if form.is_valid():
         form.save()
+        form = ProductForm()
+        #return HttpResponseRedirect("../products/product_list_view")
 
     print(request.POST)
     context = {
